b361c8fbf6d7183b73654d4c6f8ce44745200194
denishun01 <slim96@my.bcit.ca>
2020-11-17 22:42:38 -0800

    Create order.html

--- winery-brocure/dev/scripts/order.js
+++ winery-brocure/dev/scripts/order.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
+    //move to order.html when clicked single wine image
     $('.gallery img').click(function () {
         window.location.href = 'order.html';
-
     });
 });
+
+$('.show-detail img').click(function () {
+    if (this.src != null) {
+        let val = this.src.substring(0, this.src.length - 4);
+        document.getElementById('detail-wine').src = val + "-l.png";
+    }
+});
+
