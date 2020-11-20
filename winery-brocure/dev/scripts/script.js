a21a40eed95907744770d9d321b15f67f79007b8
denishun01 <slim96@my.bcit.ca>
2020-11-19 18:11:26 -0800

    revised header height & animation

--- winery-brocure/dev/scripts/script.js
+++ winery-brocure/dev/scripts/script.js
@@ -23,15 +23,32 @@ $(function () {
 }); // jQuery load
 
 //Check to see if the window is top if not then display button
+//& change header height and logo size
 $(window).scroll(function () {
+
     var height = $(window).scrollTop();
+
     if (height > 100) {
+
         $('.go-to-up').fadeIn();
+        $('.full-site').css('height', '100px');
+        $('.header-logo img').attr('src', 'images/white-logo-medium.png');
+        $('.menu-has-children').mouseenter(function () {
+            $('.sub-menu').css('background', '#f5f5f5');
+        });
+
     } else {
+
         $('.go-to-up').fadeOut();
+        $('.full-site').css('height', '200px');
+        $('.header-logo img').attr('src', 'images/white-logo.png');
+        $('.menu-has-children').mouseenter(function () {
+            $('.sub-menu').css('background', '#fff');
+        });
     }
 });
 
+//When clicked g-oup-to button
 $(document).ready(function () {
     $(".go-to-up").click(function (event) {
         event.preventDefault();
